90b8c2dc66bd46d74f72b925e67412adef6b731e
sibu64 <simon-64@hotmail.com>
2018-08-07 15:51:51 +0200

    Update Player.swift

--- FightingGame/Player/Player.swift
+++ FightingGame/Player/Player.swift
@@ -3,7 +3,7 @@
 //  FightingGame
 //
 //  Created by Darrieumerlou on 06/08/2018.
-//  Copyright © 2018 OpenClassRoom. All rights reserved.
+//  Copyright © 2018 Darrieumerlou. All rights reserved.
 //
 
 import Foundation
